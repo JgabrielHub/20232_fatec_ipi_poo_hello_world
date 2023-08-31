d1dacf0d87ffaa3c4c69f2bcce366e4c7e312145
João Gabriel <jgabriel.ferreira20@gmail.com>
2023-08-31 15:46:35 -0300

    ap01(java): hello world
    - Utilizando a linha de comando

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+    public static void main(String [] args){
+        System.out.println("Hello, World");
+        }
+    }
+    // Java o copilador pruduz bytecode
+    // é a linguagem que a JVM entende
+    // A JVM interpreta bytecode
